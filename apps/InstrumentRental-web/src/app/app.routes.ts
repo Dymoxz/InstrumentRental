57345ca9f813b0ea5c6b4b422f7fe39a6f54d32f
dymok <dymokilan@gmail.com>
2024-11-27 20:23:14 +0100

    moved profile page to backend libs

--- apps/InstrumentRental-web/src/app/app.routes.ts
+++ apps/InstrumentRental-web/src/app/app.routes.ts
@@ -1,15 +1,14 @@
 import { Route } from '@angular/router';
 import { AboutComponent } from './components/about/about.component';
 import { HomeComponent } from './components/home/home.component';
-import { InstrumentPageComponent, InstrumentDetailComponent } from '@instrument-rental/features';
-import { ProfileComponent } from './components/profile/profile.component';
+import { InstrumentPageComponent, InstrumentDetailComponent, UserProfileComponent } from '@instrument-rental/features';
 
 
 export const appRoutes: Route[] = [
   {path: 'home', component: HomeComponent},
   {path: 'about', component: AboutComponent},
   {path: 'rent', component: InstrumentPageComponent},
-  {path: 'profile', component: ProfileComponent},
+  {path: 'profile', component: UserProfileComponent},
   { path: 'instrument/:id', component: InstrumentDetailComponent },
 
 ];
