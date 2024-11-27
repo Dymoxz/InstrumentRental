e336dbd0581f3cbd3d7eaa2f0d6182624e542796
dymok <dymokilan@gmail.com>
2024-11-27 21:50:20 +0100

    fixed url issue in production env

--- libs/shared/util-env/src/lib/env.production.ts
+++ libs/shared/util-env/src/lib/env.production.ts
@@ -1,6 +1,6 @@
 import { IEnv } from './env.interface';
 
 export const env: IEnv = {
-  production: false,
-  dataApiUrl: 'instrumentrental-api-d2f7ahfvh8d2ahd0.westeurope-01.azurewebsites.net/api'
+  production: true,
+  dataApiUrl: 'https://instrumentrental-api-d2f7ahfvh8d2ahd0.westeurope-01.azurewebsites.net/api'
 }
