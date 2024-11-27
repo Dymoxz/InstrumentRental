d2da72a775e16fd8215537c887044b288dd32c38
dymok <dymokilan@gmail.com>
2024-11-27 21:42:10 +0100

    added /api to env

--- libs/shared/util-env/src/lib/env.development.ts
+++ libs/shared/util-env/src/lib/env.development.ts
@@ -2,5 +2,5 @@ import { IEnv } from './env.interface';
 
 export const env: IEnv = {
   production: false,
-  dataApiUrl: 'https://localhost:3000'
+  dataApiUrl: 'https://localhost:3000/api'
 }
--- libs/shared/util-env/src/lib/env.production.ts
+++ libs/shared/util-env/src/lib/env.production.ts
@@ -2,5 +2,5 @@ import { IEnv } from './env.interface';
 
 export const env: IEnv = {
   production: false,
-  dataApiUrl: 'instrumentrental-api-d2f7ahfvh8d2ahd0.westeurope-01.azurewebsites.net'
+  dataApiUrl: 'instrumentrental-api-d2f7ahfvh8d2ahd0.westeurope-01.azurewebsites.net/api'
 }
