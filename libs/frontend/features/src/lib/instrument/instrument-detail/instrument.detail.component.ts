7f694130d6d2cdbe53b0cc9ef02c01fad3570e2b
dymok <dymokilan@gmail.com>
2024-11-27 18:30:59 +0100

    fixed detail page showing data

--- libs/frontend/features/src/lib/instrument/instrument-detail/instrument.detail.component.ts
+++ libs/frontend/features/src/lib/instrument/instrument-detail/instrument.detail.component.ts
@@ -2,6 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { IInstrument } from '@InstrumentRental/shared/api';
 import { Subscription } from 'rxjs';
 import { InstrumentService } from '../instrument.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'lib-instrument-detail',
@@ -13,13 +14,17 @@ export class InstrumentDetailComponent implements OnInit, OnDestroy {
   instrument: IInstrument | null = null;
   subscription: Subscription | undefined = undefined;
 
-  constructor(private instrumentService: InstrumentService) {}
+  constructor(
+    private instrumentService: InstrumentService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    const instrumentId = this.route.snapshot.paramMap.get('id');
     this.subscription = this.instrumentService
-      .read('0')
+      .read(instrumentId)
       .subscribe((result: IInstrument | null) => {
-        console.log(`result: ${result}`);
+        console.log(`result: ${result?.name}`);
         this.instrument = result;
       });
   }
