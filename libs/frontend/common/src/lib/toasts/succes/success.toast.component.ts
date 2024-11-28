ad6f6713aad65200254d857de714b613db23945f
dymok <dymokilan@gmail.com>
2024-11-28 16:24:06 +0100

    updated success toast to match danger toast

--- libs/frontend/common/src/lib/toasts/succes/success.toast.component.ts
+++ libs/frontend/common/src/lib/toasts/succes/success.toast.component.ts
@@ -1,7 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'lib-delete-modal',
+  selector: 'lib-success-toast',
   templateUrl: './success.toast.component.html',
+  styleUrls: ['./success.toast.component.css']
 })
-export class SuccessToastComponent {}
+export class SuccessToastComponent {
+  @Input() toastText =  '';
+  isVisible = false;
+
+  showToast() {
+    this.isVisible = true;
+    setTimeout(() => {
+      this.isVisible = false;
+    }, 3000);
+  }
+}
