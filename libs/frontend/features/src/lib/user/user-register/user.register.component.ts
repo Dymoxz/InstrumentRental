ae59fac76c72046377e0b3d5fd743bea23c77206
dymok <dymokilan@gmail.com>
2024-12-05 13:08:17 +0100

    added auto-login to register fuction and correct data on profile page

--- libs/frontend/features/src/lib/user/user-register/user.register.component.ts
+++ libs/frontend/features/src/lib/user/user-register/user.register.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { UserService } from '../user.service';
-import { Address, Gender, IUserInfo } from '@InstrumentRental/shared/api';
+import { Address, Gender, IUserInfo, IUserCredentials } from '@InstrumentRental/shared/api';
 
 @Component({
   selector: 'lib-register',
@@ -11,43 +11,59 @@ export class UserRegisterComponent {
   lastName = '';
   email = '';
   password = '';
-  streetName ='';
-  houseNumber =  '';
+  streetName = '';
+  houseNumber = '';
   postalCode = '';
-  city =  '';
-  country =  '';
-  gender = Gender.other
+  city = '';
+  country = '';
+  bio = '';
+  phoneNumber = '';
+  gender = Gender.other;
 
-  constructor(private userService: UserService) {
-  }
+  constructor(private userService: UserService) {}
 
   onSubmit(): void {
-  const address: Address = {
-    streetName: this.streetName,
-    houseNumber: this.houseNumber,
-    postalCode: this.postalCode,
-    city: this.city,
-    country: this.country
-  };
+    const address: Address = {
+      streetName: this.streetName,
+      houseNumber: this.houseNumber,
+      postalCode: this.postalCode,
+      city: this.city,
+      country: this.country
+    };
 
-  const credentials: Omit<IUserInfo, '_id'> = {
-    address: address,
-    bio: '',
-    gender: this.gender,
-    phoneNumber: '',
-    firstName: this.firstName,
-    lastName: this.lastName,
-    email: this.email,
-    password: this.password,
-  };
+    const credentials: Omit<IUserInfo, '_id'> = {
+      address: address,
+      bio: this.bio,
+      gender: this.gender,
+      phoneNumber: this.phoneNumber,
+      firstName: this.firstName,
+      lastName: this.lastName,
+      email: this.email,
+      password: this.password,
+    };
 
-  this.userService.register(credentials).subscribe(
-    (response) => {
-      console.log('Register successful', response);
-    },
-    (error) => {
-      console.error('Register failed', error);
-    }
-  );
-}
+    this.userService.register(credentials).subscribe(
+      (response) => {
+        console.log('Register successful', response);
+        this.loginAfterRegister({ email: this.email, password: this.password });
+      },
+      (error) => {
+        console.error('Register failed', error);
+      }
+    );
+  }
+
+  private loginAfterRegister(credentials: IUserCredentials): void {
+    this.userService.login(credentials).subscribe(
+      (response) => {
+        console.log('Login successful', response);
+        console.log('token: ', response.token);
+        localStorage.setItem('token', response.token!);
+        window.location.href = '/';
+      },
+      (error) => {
+        console.error('Login after register failed', error);
+      }
+    );
+  }
 }
