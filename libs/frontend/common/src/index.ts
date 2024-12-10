748987dec5545492ea5d4ecec36025755b79021c
dymok <dymokilan@gmail.com>
2024-12-10 16:44:12 +0100

    created image placeholder component

--- libs/frontend/common/src/index.ts
+++ libs/frontend/common/src/index.ts
@@ -8,3 +8,6 @@ export * from './lib/modals/delete.modal.component'
 export * from './lib/toasts/danger/danger.toast.component'
 export * from './lib/toasts/warning/warning.toast.component'
 export *from './lib/toasts/succes/success.toast.component'
+
+/*placeholders*/
+export * from './lib/placeholders/image/image.placeholder.component'
